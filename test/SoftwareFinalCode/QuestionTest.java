104a0e015b819fb3f79f699a8ea6e4f531530955
Admin <Admin@Admin>
2020-04-11 10:27:42 -0400

    Merge origin/ProgrammingCode
    Conflicts:
    	nbproject/project.properties

---
+++ test/SoftwareFinalCode/QuestionTest.java
@@ -0,0 +1,148 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SoftwareFinalCode;
+
+import org.junit.Before;
+import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+/**
+ *
+ * @author Admin
+ */
+public class QuestionTest {
+    
+    public QuestionTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of getQuestion method, of class Question.
+     */
+    @Test
+    public void testGetQuestion() {
+        System.out.println("getQuestion");
+        Question instance = new Question();
+        String expResult = "";
+        String result = instance.getQuestion();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getOpt1 method, of class Question.
+     */
+    @Test
+    public void testGetOpt1() {
+        System.out.println("getOpt1");
+        Question instance = new Question();
+        String expResult = "";
+        String result = instance.getOpt1();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getOpt2 method, of class Question.
+     */
+    @Test
+    public void testGetOpt2() {
+        System.out.println("getOpt2");
+        Question instance = new Question();
+        String expResult = "";
+        String result = instance.getOpt2();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getOpt3 method, of class Question.
+     */
+    @Test
+    public void testGetOpt3() {
+        System.out.println("getOpt3");
+        Question instance = new Question();
+        String expResult = "";
+        String result = instance.getOpt3();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setOpt1 method, of class Question.
+     */
+    @Test
+    public void testSetOpt1() {
+        System.out.println("setOpt1");
+        String opt1 = "";
+        Question instance = new Question();
+        instance.setOpt1(opt1);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setOpt2 method, of class Question.
+     */
+    @Test
+    public void testSetOpt2() {
+        System.out.println("setOpt2");
+        String opt2 = "";
+        Question instance = new Question();
+        instance.setOpt2(opt2);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setOpt3 method, of class Question.
+     */
+    @Test
+    public void testSetOpt3() {
+        System.out.println("setOpt3");
+        String opt3 = "";
+        Question instance = new Question();
+        instance.setOpt3(opt3);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getUserChoice method, of class Question.
+     */
+    @Test
+    public void testGetUserChoice() {
+        System.out.println("getUserChoice");
+        Question instance = new Question();
+        String expResult = "";
+        String result = instance.getUserChoice();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setUserChoice method, of class Question.
+     */
+    @Test
+    public void testSetUserChoice() {
+        System.out.println("setUserChoice");
+        String userChoice = "";
+        Question instance = new Question();
+        instance.setUserChoice(userChoice);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
